a2a41ceb4626a19a38ebda61409eed47a9905502
hyberion <hyberion@gmail.com>
2024-09-07 22:13:46 -0400

    First Push, but not the last.  All hail the Omnisiah.

---
+++ adventure-log.py
@@ -0,0 +1,6 @@
+def greet_coding_temple():
+    print("Are you ready kids?  Are you ready for nautical nonesense? Do you know the emperor protects?")
+
+if __name__ == "__main__":
+    greet_coding_temple()
+    
