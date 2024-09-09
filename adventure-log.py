e34c851b266f597dc35daf08a9539d69c6bec039
hyberion <hyberion@gmail.com>
2024-09-09 19:54:57 -0400

    updated adventure-log.py

--- adventure-log.py
+++ adventure-log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Are you ready kids?  Are you ready for nautical nonesense? Do you know the emperor protects?")
+    print("Hello Navigators.  The Emperor Protects?")
 
 if __name__ == "__main__":
     greet_coding_temple()
